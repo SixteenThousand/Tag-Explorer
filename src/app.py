191ee42b5767086de852c50e6c1f1eab0a9efa66
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2023-12-07 22:01:42 +0000

    First commit

---
+++ src/app.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+def run():
+	# the entrypint of the whole application
+	root = tk.Tk()
+	root.title("Tag Explorer")
+	frame = ttk.Frame(root)
+	root.mainloop()
