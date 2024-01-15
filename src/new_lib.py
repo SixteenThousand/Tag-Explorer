9c09af727288b2f5e371e988dc6e33ae8d6e3fcd
Thomas Lindop <132360961+SixteenThousand@users.noreply.github.com>
2024-01-15 11:24:38 +0000

    >> added help buttons to new_lib.py > help buttons read help files correctly > seems to have various styling bugs now

--- src/new_lib.py
+++ src/new_lib.py
@@ -5,6 +5,8 @@ import backend
 import widgets as wg
 
 
+assets = "../assets/"
+
 root = tk.Tk()
 root.title("New Library")
 frame = ttk.Frame(root)
@@ -15,12 +17,18 @@ auto_tags_la = ttk.Label(frame,text="Automatically generate tags?")
 auto_tags_var = tk.IntVar()  # will be 1 if button is ticked, 0 otheriwse
 auto_tags_cb = ttk.Checkbutton(frame,variable=auto_tags_var)
 
+auto_tags_h = wg.HelpButton(frame,assets+"auto-tags-help")
+
 shelves_cl = wg.CheckList(frame,"Shelves:",200,150)
+
+shelves_h = wg.HelpButton(frame,assets+"shelves-help")
 
 info_rgx_se = wg.SearchEntry(
 	frame,
 	"Information regex\n(leave blank if you do not want to use this)",
 )
+
+info_rgx_h = wg.HelpButton(frame,assets+"info-regex-help")
 
 create_bu = ttk.Button(frame,text="Create New Library")
 
@@ -31,8 +39,11 @@ def populate():
 	dir_se.position(0,0)
 	utils.put(auto_tags_la,1,0,sticky="ne")
 	utils.put(auto_tags_cb,1,1,sticky="nw")
+	auto_tags_h.put(1,2)
 	shelves_cl.position(2,0)
+	shelves_h.put(2,2)
 	info_rgx_se.position(3,0)
+	info_rgx_h.put(3,2)
 	utils.put(create_bu,4,0,columnspan=3)
 	
 	for child in frame.winfo_children():
